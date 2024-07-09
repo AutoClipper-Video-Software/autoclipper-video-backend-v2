ba4fce1c4e36d5818a6aa05b0c3ca9e9f57247cb
Timóteo <randomthecoder1557@gmail.com>
2024-07-09 13:12:36 -0300

    feat(audio): create function to generate audio transcript
    The function uses an AI model called WhisperX to generate the transcript
    JSON.

--- scripts/audio.py
+++ scripts/audio.py
@@ -1,6 +1,7 @@
 import pytube
 import random
 import os
+import whisperx
 
 def download_audio_from_youtube_url(url: str) -> str:
     '''Returns the audio file path if the audio was downloaded successfully.
@@ -19,3 +20,37 @@ def download_audio_from_youtube_url(url: str) -> str:
     
     except Exception:
         raise
+
+def create_transcript_from_audio(audio_file: str, run_on_gpu: bool) -> str:
+    '''Uses WhisperX (https://github.com/m-bain/whisperx.git) to get the transcript of
+    the audio file and and writes that to a JSON file. The path to the generated JSON file
+    is returned.'''
+
+    compute_type = "int8"
+
+    if run_on_gpu:
+        compute_type = "float16"
+
+    device = "cuda"
+
+    model = whisperx.load_model('base', device=device, compute_type=compute_type)
+    audio = whisperx.load_audio(audio_file)
+    result = model.transcribe(audio, batch_size=16)
+
+    model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+    transcription = str(whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False))
+
+    transcripts_folder = 'transcripts'
+
+    if not os.path.exists(transcripts_folder):
+        os.makedirs(transcripts_folder)
+
+    random_file_id = random.randint(0, 1000)
+    transcript_file_name = f'transcript{random_file_id}.json'
+
+    transcript_file_path = os.path.join(transcripts_folder, transcript_file_name)
+
+    with open(transcript_file_path, 'w') as f:
+        f.write(transcription)
+
+    return transcript_file_path

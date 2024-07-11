35cc5ed49ca9d7fe0df0f471f6c37930435ceecb
tintinthedev <randomthecoder1557@gmail.com>
2024-07-11 14:28:28 -0300

    fix(main): fix syntax error in main.py
    Code from main.py was also written in a "prettier" way. This was done automatically by an extension.

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from Types.types import ClipTimestamp
 from scripts.video_editor import create_clip_from_video
 from scripts.video_downloader import download_youtube_video
 from moviepy.editor import VideoFileClip
-from scripts.audio import get_audio_from_clip, move_audio_to_correct_folder, transcribe_audio
+from scripts.audio import (
+    get_audio_from_clip,
+    move_audio_to_correct_folder,
+    transcribe_audio,
+)
 from os import listdir
 from os.path import isfile, join
 
@@ -26,10 +30,12 @@ for timestamp in clips_timestamps:
 
 for clip in clips:
     clip_audio = get_audio_from_clip(clip)
-    move_audio_to_correct_folder(clip_audio)`
+    move_audio_to_correct_folder(clip_audio)
 
 audio_files = [f for f in listdir("audios") if isfile(join("audios", f))]
 
 for audio_file in audio_files:
-    audio_transcript = transcribe_audio(audio_file, run_on_gpu=False) # run_on_gpu must be True when in production
+    audio_transcript = transcribe_audio(
+        audio_file, run_on_gpu=False
+    )  # run_on_gpu must be True when in production
     print(audio_transcript)

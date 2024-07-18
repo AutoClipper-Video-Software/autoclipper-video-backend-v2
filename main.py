92950c8a0c93929ebcf9f960d0cee9e19acba6c7
tintinthedev <randomthecoder1557@gmail.com>
2024-07-18 14:26:04 -0300

    fix merging conflict

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 from Types.types import ClipTimestamp
-<<<<<<< HEAD
-import json
-from scripts.video_editor import create_clip_from_video
-from scripts.video_downloader import download_youtube_video
-from moviepy.editor import VideoFileClip
-from scripts.audio import (
-    get_audio_from_clip,
-    move_audio_to_correct_folder,
-    transcribe_audio,
-=======
 from downloader import download_audio_from_youtube_video, download_youtube_video
 from editor import (
     merge_video_and_audio,
@@ -16,7 +6,6 @@ from editor import (
     resize_clip,
     create_video_from_clip,
     add_captions_to_clip,
->>>>>>> feat/implement-video-edition
 )
 from helpers.delete_files import delete_files
 
@@ -38,18 +27,6 @@ for timestamp in clips_timestamps:
 
     resized_clip = resize_clip(clip)
 
-<<<<<<< HEAD
-audio_files = [file for file in listdir("audios") if isfile(join("audios", file))]
-
-for audio_file in audio_files:
-    audio_file_path = f"audios/{audio_file}"
-
-    # run_on_gpu must be True when in production
-    audio_transcript = transcribe_audio(audio_file=audio_file_path, run_on_gpu=False)
-
-    with open(audio_transcript) as f:
-        subtitles_json = json.load(f)
-=======
     videoclip = create_video_from_clip(resized_clip)
 
     add_captions_to_clip(videoclip)
@@ -57,4 +34,3 @@ for audio_file in audio_files:
     delete_files([videoclip])
 
 delete_files([video_path, audio_path, video_with_audio])
->>>>>>> feat/implement-video-edition
